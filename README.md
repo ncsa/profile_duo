02ae6af36b7829238d2005dbbd50c747a288a1c5
Bill Glick <bglick@illinois.edu>
2022-02-28 16:24:47 -0600

    ASDPLNG-126: Add basic duosecurity/duo_unix module functionality into this profile
    Get working with NCSA environment
    Add kerberos options for sshd in README
    Add a note about sshd custom_cfg for profile_allow_ssh_from_bastion
    Add notes about using hostnames that do not match hostname -f

--- README.md
+++ README.md
@@ -20,6 +20,9 @@ This puppet profile customizes a host to install and configure Duo 2FA
 
 See https://duo.com/docs/duounix
 
+Note: This module is highly based off the no longer supported module from Duo:
+- [duosecurity/duo_unix](https://forge.puppet.com/modules/duosecurity/duo_unix)
+
 
 ## Setup
 
@@ -31,19 +34,37 @@ include ::profile_duo
 
 ## Usage
 
-The following parameters will need to be set via Hiera (preferably via Vault):
+The following parameters will need to be set via Hiera (preferably the keys should come from Vault):
 ```
-duo_unix::ikey: "ikey_obtained_from_duo_admins"
-duo_unix::skey: "skey_obtained_from_duo_admins"
+profile_duo::host: "duo_host_obtained_from_duo_admins"
+profile_duo::ikey: "ikey_obtained_from_duo_admins"   # STORE IN VAULT INSTEAD OF YAML
+profile_duo::skey: "skey_obtained_from_duo_admins"   # STORE IN VAULT INSTEAD OF YAML
+sshd::config:
+  AuthenticationMethods: "gssapi-with-mic,keyboard-interactive:pam password,keyboard-interactive:pam"
+  ChallengeResponseAuthentication: "yes"
+  GSSAPIStrictAcceptorCheck: "no"
+  KerberosAuthentication: "yes"
 ```
+
 See [Protecting your system or application with NCSA Duo](https://wiki.ncsa.illinois.edu/display/cybersec/Duo+at+NCSA#DuoatNCSA-ProtectingyoursystemorapplicationwithNCSADuo) for instructions on obtaining keys for your specific host(s).
 
+If making use of [ncsa/profile_allow_ssh_from_bastion](https://github.com/ncsa/puppet-profile_allow_ssh_from_bastion) or similar profiles, you will want to set the following in hiera:
+```
+profile_allow_ssh_from_bastion::custom_cfg:
+  AuthenticationMethods: "gssapi-with-mic,keyboard-interactive:pam password,keyboard-interactive:pam"  ## NOT NEEDED IN sshd::config HASH
+  DisableForwarding: "no"
+  GSSAPIAuthentication: "yes"
+  KerberosAuthentication: "yes"  ## NOT NEEDED IN sshd::config HASH
+  MaxAuthTries: "6"
+  PasswordAuthentication: "yes"  
+  PubkeyAuthentication: "no"
+``` 
+
+If you want GSSAPI authentication to work with Kerberos tickets, you need to make sure that you have a Kerberos host principal in the default keytab file that matches the fully qualified domain name of the hostname that users log into.
 
 ## Dependencies
 
-- [duosecurity/duo_unix](https://forge.puppet.com/modules/duosecurity/duo_unix)
 - [herculesteam/augeasproviders_pam](https://forge.puppet.com/modules/herculesteam/augeasproviders_pam)
-- [ncsa/pam_access](https://github.com/ncsa/puppet-pam_access)
 
 
 ## Reference
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,49 @@
 # @summary Install and manage Duo auth for SSH with PAM
 #
+# @param accept_env_factor,
+#   Duo configuration for 'accept_env_factor'
+#
+# @param autopush,
+#   Duo configuration for 'autopush'
+#
+# @param failmode,
+#   Duo configuration for 'failmode'
+#
+# @param fallback_local_ip,
+#   Duo configuration for 'fallback_local_ip', defaults to 'no'
+#
+# @param group,
+#   Duo configuration for 'groups' Group restriction
+#
+# @param http_proxy,
+#   Duo configuration for 'http_proxy' HTTP proxy setting
+#
+# @param host,
+#   Duo API host
+#
+# @param ikey,
+#   Duo integration key
+#
+# @param motd,
+#   Duo configuration for 'motd' MOTD display (only applies if $usage = 'login')
+#
+# @param package,
+#
+# @param pam_config,
+#   pam resource parameters for setting up duo pam configurations
+#
+# @param prompts,
+#   Duo configuration for 'prompts'
+#
+# @param pushinfo,
+#   Duo configuration for 'pushinfo'
+#
+# @param skey,
+#   Duo secret key
+#
+# @param usage,
+#   Duo usage method - defaults to 'pam'
+#
 # @param yumrepo_baseurl
 #   baseurl of yumrepo used to install duo_unix package
 #
@@ -12,16 +56,31 @@
 # @param yumrepo_name
 #   name of yumrepo used to install duo_unix package
 #
-# @param pam_config
-#   pam resource parameters for setting up duo pam configurations
-#
 class profile_duo (
+  String $accept_env_factor,
+  String $autopush,
+  String $failmode,
+  String $fallback_local_ip,
+  String $group,
+  String $http_proxy,
+  String $host,
+  String $ikey,
+  String $motd,
+  String $package,
+  Hash   $pam_config,
+  String $prompts,
+  String $pushinfo,
+  String $skey,
+  String $usage,
   String $yumrepo_baseurl,
   String $yumrepo_description,
   String $yumrepo_gpgkey,
   String $yumrepo_name,
-  Hash   $pam_config,
 ) {
+
+  if $ikey == '' or $skey == '' or $host == '' {
+    fail('ikey, skey, and host must all be defined.')
+  }
 
   ## install Pakrat Yum repo for Duo
   yumrepo { $yumrepo_name:
@@ -33,16 +92,57 @@ class profile_duo (
     gpgkey   => $yumrepo_gpgkey,
   }
 
-  include ::duo_unix
+  ## INSTALL $package
+  package {  $package:
+    ensure  => 'installed',
+    require => [
+      Yumrepo[$yumrepo_name],
+    ],
+  }
 
-  # CONFIGURE PAM STACK (BORROWED FROM LSST: stdcfg::access)
-  ## The duo_unix module does NOT manage the PAM stack correctly.
-  ## We do it our own way, based on what is documented by Duo Security:
-  ##    https://duo.com/docs/duounix
-  each($pam_config) |String[1] $key, Hash $value| {
-    pam { $key:
-      * => $value,
+  file { '/usr/sbin/login_duo':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '4755',
+    require => Package[$package];
+  }
+
+  if $usage == 'pam' {
+    ## CONFIGURE pam_duo
+    file { '/etc/duo/pam_duo.conf':
+      ensure  => present,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0600',
+      content => template('profile_duo/duo.conf.erb'),
+      require => Package[$package];
     }
+
+    # CONFIGURE PAM STACK (BORROWED FROM LSST: stdcfg::access)
+    ## The duo_unix module does NOT manage the PAM stack correctly.
+    ## We do it our own way, based on what is documented by Duo Security:
+    ##    https://duo.com/docs/duounix
+    each($pam_config) |String[1] $key, Hash $value| {
+      pam { $key:
+        * => $value,
+      }
+    }
+  }
+  else
+  {
+    ## $usage == 'login'
+    fail('usage = \'login\' is not supported.')
+
+    #file { '/etc/duo/login_duo.conf':
+    #  ensure  => present,
+    #  owner   => 'root',
+    #  group   => 'root',
+    #  mode    => '0600',
+    #  content => template('duo_unix/duo.conf.erb'),
+    #  require => Package[$package];
+    #}
+
   }
 
 }
